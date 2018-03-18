63d5c34ea3a65da02fd27cedd54208531e9973c8
hafa786 <hafizm.shehzad@gmail.com>
2018-03-18 21:36:09 +0200

    modifying basics concept

---
+++ NodeJsProjects/JavescriptBasics.js
@@ -0,0 +1,16 @@
+/**
+ * /object storing data
+ */
+var print  = function () {
+
+    console.log("Hello Friends");
+
+}
+/**
+ * calling
+ */
+print();
+/**
+ * with timer
+ */
+setTimeout(print, 3000);
