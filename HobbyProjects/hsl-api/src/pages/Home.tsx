95782dc00c4a6c4748be8ac9791e63cffccffa13
Hafiz <hafizmuhammadshahzad.x.sikandar@questdiagnostics.com>
2025-02-12 13:42:13 +0200

    added the search form to home page

--- HobbyProjects/hsl-api/src/pages/Home.tsx
+++ HobbyProjects/hsl-api/src/pages/Home.tsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import '../styles/global.scss'; // Import SCSS file
+import { TextInput } from '../components/inputs';
+import { PrimaryButton, TextButton } from '../components/buttons';
 
-const Home = () => {
-  return <h1>Home Page</h1>;
+const RouteSearch: React.FC = () => {
+  return (
+    <div className="route-search-form">
+      <div className="search-form">
+        <TextInput
+          lable=""
+          type="text"
+          placeholder="Käytä nykyistä sijaintia"
+          required
+        />
+        <TextInput
+          lable=""
+          type="text"
+          placeholder="Syötä määränpää"
+          required
+        />
+        <div className="actions">
+          <TextButton onClick={() => null} text={'Cancel'} />
+          <PrimaryButton onClick={() => null} text={'Käytä'} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Home: React.FC = () => {
+  return (
+    <div className="home">
+      <RouteSearch />
+    </div>
+  );
 };
 
 export default Home;
