bc5ffbf8116f3e4ad5ca745e196f881967affb98
Hafiz <hafizmuhammadshahzad.x.sikandar@questdiagnostics.com>
2025-02-12 13:32:53 +0200

    update the App.tsx

--- HobbyProjects/hsl-api/src/App.tsx
+++ HobbyProjects/hsl-api/src/App.tsx
@@ -2,17 +2,19 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
-import Navbar from './components/Navbar';
+import Header from './components/header';
+import Footer from './components/footer';
 
 const App = () => {
   return (
     <Router>
-      <Navbar />
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <Footer />
     </Router>
   );
 };
